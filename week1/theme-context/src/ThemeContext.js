85d06574a8d1a4c4fc047bb593ac7f8f2005dd0f
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-11-20 08:44:13 -0600

    themeContextAssignment

---
+++ week1/theme-context/src/ThemeContext.js
@@ -0,0 +1,25 @@
+import React, {useContext, useState} from 'react'
+
+const ThemeContext = React.createContext()
+const ThemeUpdateContext = React.createContext()
+
+export function useTheme(){ 
+    return useContext(ThemeContext) // Uses Theme ----- Custom Hook
+}
+
+export function useThemeUpdate(){
+    return useContext(ThemeUpdateContext) // Updates The Theme ---- Custom Hook
+}
+
+export function ThemeProvider ({children}) {
+    const [darkTheme, SetDarkTheme] = useState(true) // Creating State 
+    const toggleTheme = () => SetDarkTheme(prevDarkTheme => !prevDarkTheme)  // Updates State
+
+    return (
+        <ThemeContext.Provider value={darkTheme}> {/* Passed Down to Children */}
+            <ThemeUpdateContext.Provider value={toggleTheme}> {/* Passed Down to Children */}
+                {children}
+            </ThemeUpdateContext.Provider>
+        </ThemeContext.Provider>
+    )
+}
